afc582b110192eafc5d99302671c75029aa800e3
romarre <romarre1@inf.upv.es>
2013-04-30 12:18:07 +0200

    Clase material

--- ProyectoGPR/src/logica/Material.java
+++ ProyectoGPR/src/logica/Material.java
@@ -1,5 +1,16 @@
 package logica;
 
-public class Material {
+import java.sql.Date;
 
+public class Material {
+	private String iD;
+	private String nombre;
+	private String descripcion;
+	private int disponibles; 
+	public Material(String _iD,String _nombre,String _descripcion,int _disponibles){
+		iD=_iD;
+		nombre=_nombre;
+		descripcion=_descripcion;
+		disponibles=_disponibles;
+	}
 }
